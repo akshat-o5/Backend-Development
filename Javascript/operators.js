5a4757d69f5c28686f2fa5f196c9247a7aafcf86
Akshat Gupta <a.akshat903@gmail.com>
2022-08-02 12:07:41 +0530

    js4.inserted

---
+++ Javascript/operators.js
@@ -0,0 +1,52 @@
+// OPERATORS IN JAVASCRIPT
+
+
+      //ARITHMETIC OPERATORS
+
+var a = 100;
+var b = 10;
+console.log("The value a + b is ", a+b);
+console.log("The value a - b is ", a-b);
+console.log("The value a * b is ", a*b);
+console.log("The value a / b is ", a/b);
+
+
+
+      //ASSIGNMENT OPERATORS
+
+var c = b;
+c += 2;
+console.log(c);
+c -= 2;
+console.log(c);
+c *= 2;
+console.log(c);
+c /= 2;   
+console.log(c);  
+
+
+
+      //COMPARISON OPERATORS
+
+var x = 34;
+var y = 56;
+console.log(x == y);
+console.log(x > y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x <= y);
+
+
+
+      //LOGICAL OPERATORS
+      
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)     
+console.log(!true) 
+console.log(!false) 
