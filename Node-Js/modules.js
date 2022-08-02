465013e34354448befc4877a2f69cc52c12b226a
Akshat Gupta <a.akshat903@gmail.com>
2022-08-03 00:44:34 +0530

    changes made

--- Node-Js/modules.js
+++ Node-Js/modules.js
@@ -5,13 +5,15 @@
 //     console.log('Hello there '+ name)
 // }
 
-const names = require('./names')
-const sayHi = require('./utils')
-const data = require('./alternative-flavour')
-console.log(data)
+// const names = require('./names')
+// const sayHi = require('./utils')
+// const data = require('./alternative-flavour')
+// console.log(data)
 // console.log(names)
+const data = require('./mind-granade')
+console.log(data)
 
 
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
