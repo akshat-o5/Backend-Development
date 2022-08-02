f5d3530e2f0effd094437847ca6b251dd21425d1
Akshat Gupta <a.akshat903@gmail.com>
2022-08-02 12:22:18 +0530

    js10 inserted

---
+++ Javascript/strings.js
@@ -0,0 +1,23 @@
+// STRING METHODS
+
+
+let mystr = "HARRY IS A GOOD BOY";
+let mystr1 = "Jhooth"
+
+console.log(mystr.length);
+
+console.log(mystr.indexOf("HARRY"));
+
+console.log(mystr.indexOf("IS"));
+
+console.log(mystr.slice(0,3));
+
+console.log(mystr.toUpperCase)
+
+console.log(mystr.toLowerCase)
+
+console.log(mystr.concat(mystr1))
+
+console.log(mystr.trim)
+
+console.log(mystr.charAt(4))
