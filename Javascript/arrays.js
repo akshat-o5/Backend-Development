363997f24ed52a47934828f7c021a4dea6f20d6e
Akshat Gupta <a.akshat903@gmail.com>
2022-08-02 12:16:59 +0530

    js9 inserted

---
+++ Javascript/arrays.js
@@ -0,0 +1,26 @@
+
+// ARRAY METHODS
+
+
+let myArr = ["FAN", "CAMERA", 34, null, true];
+console.log(myArr.length);
+
+myArr.pop();
+console.log(myArr);
+
+myArr.push("HARRY");
+console.log(myArr);
+
+myArr.shift();
+console.log(myArr);
+
+myArr.unshift("Akshat");
+console.log(myArr);
+
+console.log(myArr.unshift());
+
+myArr.sort();
+console.log(myArr);
+
+myArr.reverse();
+console.log(myArr)
