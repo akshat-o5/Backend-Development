88ad22de292c4e486306c9289c13d76ef805ac42
Akshat Gupta <a.akshat903@gmail.com>
2022-08-02 12:05:40 +0530

    js3 inserted

---
+++ Javascript/data-types.js
@@ -0,0 +1,65 @@
+// // DATA TYPES IN JAVASCRIPTS
+
+
+
+//         //NUMBERS
+
+var num1 = 43
+
+var num2 = 77.89
+
+console.log(num1);
+
+
+
+//         //STRINGS 
+
+var str1 = "This is a string";
+
+var str2 = "This is also a string";
+
+console.log(str1);
+
+
+//         // OBJECTS
+
+var marks = {
+    ravi: 34,
+    shubham: 98,
+    harry: 99.99 
+}        
+
+console.log(marks);
+
+
+//        //BOOLEANS
+
+var a = true;
+var b = false;       
+
+console.log(a)
+console.log(b)
+
+
+
+//        // UNDEFINED
+
+var und = undefined; 
+
+console.log(und)
+
+
+
+//        // NULL
+
+var z = null;  
+
+console.log(z)
+
+
+
+//        // ARRAY
+
+var arr = [1, 2, "akshat", 4, 5]; 
+
+console.log(arr)
