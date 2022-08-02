69a67d3e777dd08710fd3b718d7ddbdc017208dc
Akshat Gupta <a.akshat903@gmail.com>
2022-08-03 00:16:55 +0530

    changes made

--- Node-Js/modules.js
+++ Node-Js/modules.js
@@ -1,9 +1,13 @@
-const john = 'john'
-const peter = 'peter'
+// const john = 'john'
+// const peter = 'peter'
 
-const sayHi = (name) =>{
-    console.log('Hello there '+ name)
-}
+// const sayHi = (name) =>{
+//     console.log('Hello there '+ name)
+// }
+
+const names = require('./names')
+console.log(names)
+
 
 sayHi('susan')
 sayHi(john)
