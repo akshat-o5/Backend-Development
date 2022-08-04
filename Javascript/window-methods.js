2d81e1d2700f00f9b2c6d10a14e1ab97609fb4f4
Akshat Gupta <a.akshat903@gmail.com>
2022-08-04 23:47:47 +0530

    window methods

---
+++ Javascript/window-methods.js
@@ -0,0 +1,37 @@
+
+// THE WINDOW OBJECT GIVES US ACCESS TO SOME METHODS LIKE:
+
+// 1) setInterval()
+// 2) setTimeout()
+// 3) alert()
+// 4) confirm()
+// 5) prompt()
+// 6) open()
+// 7) close()
+
+
+
+
+// alert
+alert('this is an alert')
+
+
+// confirm
+// IT GIVES US TWO BUTTONS 'OK' AND 'CANCEL'. 
+//WHEN WE CLICK 'OK', IT RETURNSS TRUE. WHEN WE CLICK ON 'CANCEL', IT RETURNS FALSE.
+confirm('Are you sure')
+
+
+// prompt
+// IT IS USED TO GET A VLUE FROM USER
+prompt('enter a number')
+
+
+// open
+// IT ACCEPTS A URL AND IT OPENS THAT URL IN A NEW BROWSER TAB
+window.open('https://www.google.com')
+
+
+// close
+// IT CLOSES THE CURRENT BROWSER TAB
+close()
