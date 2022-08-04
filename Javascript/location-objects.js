36bd7c0774eb0742b967412d46710cd8dcc0eb30
Akshat Gupta <a.akshat903@gmail.com>
2022-08-05 00:15:48 +0530

    location objects

---
+++ Javascript/location-objects.js
@@ -0,0 +1,34 @@
+
+// THE LOCATION OBJECT CAN BE USED TO GET THE DETAILS OF THE CURRENT PAGE ADDRESS. IT CAN ALSO REDIRECT THE BROWSER TO A NEW PAGE IN THE SAME BROWSER TAB.
+
+
+
+
+// TO ACTIVATE LOCARION OBJECT
+window.location
+
+
+
+
+window.location.href
+
+
+
+
+window.location.hostname
+
+
+
+
+window.location.pathname
+
+
+
+
+window.location.search
+
+
+
+
+
+window.location.assign('https://www.google.com')
