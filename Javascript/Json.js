1c8f961a859aaa6b4c76c1a1f8cd6dac5fa22663
Akshat Gupta <a.akshat903@gmail.com>
2022-08-05 23:09:05 +0530

    updated

--- Javascript/Json.js
+++ Javascript/Json.js
@@ -3,65 +3,65 @@
 // IT IS BASICALLY A TEXT FORMAT THAT MAKES IT EASY TO SHARE DATA BETWEEN CLIENTS AND SERVERS. IT'S ORIGIN IS BASED ON HOW JAVASCRIPT OBJECT WORKS SO THAT'A WHY IT LOOKS LIKE A JAVASCRIPT OBJECT.
 
 
-// // to create a json object
-// var mobj = {
-//     'name': 'John',
-//     'age': 28
-// }
+// to create a json object
+var mobj = {
+    'name': 'John',
+    'age': 28
+}
 
-// // to access it's elemnts
-// console.log(mobj.age)
-// console.log(mobj.name)
-
-
-// // to create a array
-// var mArr = [1, 2, 3, 4, 5]
-
-// // to access it's elemnts
-// console.log(mArr[0])
-// console.log(mArr[1])
-// console.log(mArr[2])
-// console.log(mArr[3])
-// console.log(mArr[4])
-// console.log(mArr[5])
+// to access it's elemnts
+console.log(mobj.age)
+console.log(mobj.name)
 
 
-// // to create multiple json objects
-// var mobj1 = [
-//     {
-//     'name': 'John',
-//     'age': 28
-// },{
-//     'name': 'Akshat',
-//     'age': 20
-// },{
-//     'name': 'Saatvik',
-//     'age': 20
-// }
-// ]    
+// to create a array
+var mArr = [1, 2, 3, 4, 5]
 
-// // to access its objects
-// console.log(mobj1[0])
-// console.log(mobj1[0].age)
-// console.log(mobj1[0].name)
+// to access it's elemnts
+console.log(mArr[0])
+console.log(mArr[1])
+console.log(mArr[2])
+console.log(mArr[3])
+console.log(mArr[4])
+console.log(mArr[5])
 
-// console.log(mobj1[1])
-// console.log(mobj1[1].age)
-// console.log(mobj1[1].name)
 
-// console.log(mobj1[2])
-// console.log(mobj1[2].age)
-// console.log(mobj1[2].name)
+// to create multiple json objects
+var mobj1 = [
+    {
+    'name': 'John',
+    'age': 28
+},{
+    'name': 'Akshat',
+    'age': 20
+},{
+    'name': 'Saatvik',
+    'age': 20
+}
+]    
 
-// console.log(mobj1[3])
-// console.log(mobj1[3].age)
-// console.log(mobj1[3].name)
+// to access its objects
+console.log(mobj1[0])
+console.log(mobj1[0].age)
+console.log(mobj1[0].name)
 
-// console.log(mobj1[4])
-// console.log(mobj1[4].age)
-// console.log(mobj1[4].name)
+console.log(mobj1[1])
+console.log(mobj1[1].age)
+console.log(mobj1[1].name)
 
-// console.log(mobj1[5])
+console.log(mobj1[2])
+console.log(mobj1[2].age)
+console.log(mobj1[2].name)
+
+console.log(mobj1[3])
+console.log(mobj1[3].age)
+console.log(mobj1[3].name)
+
+console.log(mobj1[4])
+console.log(mobj1[4].age)
+console.log(mobj1[4].name)
+
+console.log(mobj1[5])
 
 
 
