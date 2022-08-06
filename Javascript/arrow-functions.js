873b30c3a0ba31b3fdf43923fe907285317532fa
Akshat Gupta <a.akshat903@gmail.com>
2022-08-06 22:24:33 +0530

    arrow-functions.js

---
+++ Javascript/arrow-functions.js
@@ -0,0 +1,13 @@
+// ES6 GIVES US A NEW SYNTAX FOR DEFINING FUNCTIONS USING A FAT ARROW. 
+
+// ARROW FUNCTIONS BRING A LOT OF CLARITY AND COS=DE REDUCTION.
+
+
+
+// const mGreetings = (name, age) =>{
+//     return `Welcome ${name} ${age}`
+// }
+
+const mGreetings = (name, age) => `WELCOME ${name} ${age}`
+
+console.log(mGreetings('Akshat', 20))
