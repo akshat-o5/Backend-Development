9ec3c287a0e6a630d91cb8c58091d1be3e2fb830
Akshat Gupta <a.akshat903@gmail.com>
2022-08-06 17:30:58 +0530

    variables-in-es6.js

---
+++ Javascript/variables-in-es6.js
@@ -0,0 +1,30 @@
+//  TO NEW WAYS :
+
+// 1) let : IT IS A REPLACEMENT FOR var.
+
+// 2) const : IT IS USED FOR DECLARING/HOLDING CONSTANT VALUES.
+
+
+
+
+let num1 = 10
+console.log(num1)
+
+
+let name = 'AKSHAT GUPTA'
+let isOldEnough = false
+let marks = [9, 10, 4, 8]
+let blogObj = {
+    title: 'Blog Title 1',
+    description: 'GOOD NIGHT'
+}
+
+
+const num2 = 20
+// num2 = 30
+console.log(num2)
+
+
+
+
+// VARIABLES CREATED BY let AND const ARE BLOCK SCOPE , AND THEY DONOT SUPPORT HOISTING.
