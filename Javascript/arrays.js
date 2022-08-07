3441ac75045898edc6f0a1885f6a4a24a768ffce
Akshat Gupta <a.akshat903@gmail.com>
2022-08-07 16:49:00 +0530

    array.js

--- Javascript/arrays.js
+++ Javascript/arrays.js
@@ -24,3 +24,32 @@ console.log(myArr);
 
 myArr.reverse();
 console.log(myArr)
+
+
+
+
+// ES6 FUNCTIONS:
+
+
+// ES6 INTRODUCED TWO NEW METODS TO SEARCH FOR ELEMENTS INSIDE THE ARRAY.
+
+// -> find(): IT IS USED TP SEATRCH FOR AN ELEMENT IN THE ARRAY THAT MATCHES SOME CONDITION. IT RETURNS THE FIRST ELEMENT THAT MATCHES THE CONDITION.
+
+// -> findIndex() : IT IS QUITE SIMILAR TO THE find() METHOD. THE DIFERENCE IS THAT findIndex() METHOD RETURNS THE INDEX OF THE ELEMENT INSTEAD OF THE ELEMENT ITSELF.
+
+
+
+
+
+const numArr = [1, 2, 3, 4, 5, 6, 7]
+
+const result = numArr.find((item) => {
+    return item%2==0
+})
+
+const result1 = numArr.findIndex((item) => {
+    return item%2==0
+})
+
+console.log(result)
+console.log(result1)
