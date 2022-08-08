3e57d0ac55bc2e71f1b22f17daaeb3ea046fceaa
Akshat Gupta <a.akshat903@gmail.com>
2022-08-08 19:06:51 +0530

    os module updated

--- Node-Js/os-module.js
+++ Node-Js/os-module.js
@@ -2,21 +2,21 @@
 // TO IMPORT OS MODULE
 const os = require('os')
 
-// // info about current user
-// const user = os.userInfo()
-// console.log(user)
+// info about current user
+const user = os.userInfo()
+console.log(user)
 
-// //method that returns the system uptime in seconds
-// console.log('the system uptime is ' ,os.uptime() ,'seconds')
+//method that returns the system uptime in seconds
+console.log('the system uptime is ' ,os.uptime() ,'seconds')
 
-// //some other methods of os modulule
-// const currentOS = {
-//     name: os.type(),
-//     release: os.release(),
-//     total_memory: os.totalmem(),
-//     free_memory: os.freemem(), 
-// }
-// console.log(currentOS)
+//some other methods of os modulule
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    total_memory: os.totalmem(),
+    free_memory: os.freemem(), 
+}
+console.log(currentOS)
 
 
 console.log(os.freemem())
