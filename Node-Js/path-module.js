85e003bdb40a5f8433d5c0d7beec6a7602763d85
Akshat Gupta <a.akshat903@gmail.com>
2022-08-08 19:20:42 +0530

    path module updated

--- Node-Js/path-module.js
+++ Node-Js/path-module.js
@@ -1,12 +1,40 @@
+
+// The node:path module provides utilities for working with file and directory paths.
+
+
+// TO ADD BUILT-IN PATH MODULE TO OUR FILE
 const path = require('path')
 
-console.log(path.sep)
+// console.log(path.sep)
 
-const filePath = path.join('/content/', 'subfolder', 'test.txt')
-console.log(filePath)
+// const filePath = path.join('/content/', 'subfolder', 'test.txt')
+// console.log(filePath)
 
-const base = path.basename(filePath)
-console.log(base)
+// const base = path.basename(filePath)
+// console.log(base)
 
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
-console.log(absolute)
+// const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+// console.log(absolute)
+
+
+
+
+
+
+// to print basename of path:
+console.log(path.basename('C:\\temp\\myfile.html'))
+
+//to print present woring directory name
+console.log(path.dirname('/foo/bar/baz/asdf/quux'))
+
+// to print extension name :
+console.log(path.extname(__filename))
+
+
+
+
+
+
+
+
+// GOTO NODEJS DOCUMENTATION TO SEE MORE DFEATURES OF OS BUIT-IN MODULES
