a953796097f33ae788474a65c8eb268155257c3e
Akshat Gupta <a.akshat903@gmail.com>
2022-08-08 19:05:33 +0530

    os-module updated

--- Node-Js/os-module.js
+++ Node-Js/os-module.js
@@ -1,17 +1,37 @@
+
+// TO IMPORT OS MODULE
 const os = require('os')
 
-// info about current user
-const user = os.userInfo()
-console.log(user)
+// // info about current user
+// const user = os.userInfo()
+// console.log(user)
 
-//method that returns the system uptime in seconds
-console.log('the system uptime is ' ,os.uptime() ,'seconds')
+// //method that returns the system uptime in seconds
+// console.log('the system uptime is ' ,os.uptime() ,'seconds')
 
-//some other methods of os modulule
-const currentOS = {
-    name: os.type(),
-    release: os.release(),
-    total_memory: os.totalmem(),
-    free_memory: os.freemem(), 
-}
-console.log(currentOS)
+// //some other methods of os modulule
+// const currentOS = {
+//     name: os.type(),
+//     release: os.release(),
+//     total_memory: os.totalmem(),
+//     free_memory: os.freemem(), 
+// }
+// console.log(currentOS)
+
+
+console.log(os.freemem())
+
+console.log(os.homedir())
+
+console.log(os.hostname())
+
+console.log(os.platform())
+
+console.log(os.release())
+
+console.log(os.type())
+
+
+
+
+// GOTO NODEJS DOCUMENTATION TO SEE MORE DFEATURES OF OS BUIT-IN MODULES
