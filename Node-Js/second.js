268de228d4fcfb8ebb8bdc67e75353743be88cc2
Akshat Gupta <a.akshat903@gmail.com>
2022-08-08 18:59:15 +0530

    updated

--- Node-Js/second.js
+++ Node-Js/second.js
@@ -8,3 +8,22 @@ info = {
 
 // IT MEANS TO ALLOW ACCESSING OF info FROM OTHER FILES AS WELL.
 module.exports = info
+
+
+
+
+
+
+
+
+
+
+
+// MODULE WRAPPER FUNCTION:
+
+// function(exports, require, module, __filename, __dirname) 
+
+// NODEJS DOES IT AUTOMATICALLY
+
+
+console.log(exports, require, module, __filename, __dirname)
