eab3e0fd5d8b52ee66515e8c1ecfbe6b4b7100e3
Akshat Gupta <a.akshat903@gmail.com>
2022-08-09 13:48:26 +0530

    index.js

---
+++ Express-Js/blog-express/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+// // CREATING OUR OWN EXPRESS MIDDLEWARE
+// const akshatMiddleware = (req, res, next) => {
+//   console.log(req)
+//   next()
+// }
+
+// USING EXPRESS MIDDLEWARES
+app.use(express.static(path.join(__dirname, "static")))
+// app.use(akshatMiddleware)
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+
+app.get("/about", (req, res) => {
+  // res.send('Hello World! about')
+  // res.sendFile(path.join(__dirname, 'index.html'))
+  // res.status(500)
+  res.json({ akshat: 34 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
