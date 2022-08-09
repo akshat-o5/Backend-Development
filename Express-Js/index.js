5f8d3fb7d08c92955f4095fd99b29dd3de993343
Akshat Gupta <a.akshat903@gmail.com>
2022-08-09 12:32:52 +0530

    files added

--- Express-Js/index.js
+++ Express-Js/index.js
@@ -1,17 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/about', (req, res) => {
-  res.send('Hello World! about')
-})
+app.get("/about", (req, res) => {
+  // res.send('Hello World! about')
+  // res.sendFile(path.join(__dirname, 'index.html'))
+  // res.status(500)
+  res.json({ akshat: 34 });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
