d90810f106a79d3aa25843c4f0a80f4c1bf93d52
Akshat Gupta <a.akshat903@gmail.com>
2022-08-09 13:39:17 +0530

    files added

--- Express-Js/index.js
+++ Express-Js/index.js
@@ -3,8 +3,18 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// // CREATING OUR OWN EXPRESS MIDDLEWARE
+// const akshatMiddleware = (req, res, next) => {
+//   console.log(req)
+//   next()
+// }
+
+// USING EXPRESS MIDDLEWARES
+app.use(express.static(path.join(__dirname, "public")))
+// app.use(akshatMiddleware)
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
 });
 
 app.get("/about", (req, res) => {
