ce72cf5bfcbb0da53520f3f5560a9a996475ae2f
Akshat Gupta <a.akshat903@gmail.com>
2022-08-10 12:08:15 +0530

    files added

---
+++ Express-Js/blog-express/data/blogs.js
@@ -0,0 +1,22 @@
+{
+    blogs:[
+        {
+            title: "How to get started with JavaScript",
+            content: "This is content JavaScript"
+        },
+        {
+            title: "How to get started with Node-Js",
+            content: "This is content Node-Js"
+        },
+        {
+            title: "How to get started with Express-Js",
+            content: "This is content Express-Js"
+        },
+        {
+            title: "How to get started with Mongo-Db",
+            content: "This is content Mongo-Db"
+        }
+    ]
+}
+
+module.exports = blogs
--- Express-Js/blog-express/routes/blog.js
+++ Express-Js/blog-express/routes/blog.js
@@ -2,9 +2,19 @@ const express = require('express')
 
 const path = require('path')
 
+const blogs = require('.../data/blogs')
+
 const router = express.Router()
 
+
 router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../templates/index.html'))
+})
+
+router.get('/blog', (req, res) => {
+    blogs.forEach(ele => {
+        console.log(ele.title)
+    });
     res.sendFile(path.join(__dirname, '../templates/index.html'))
 })
 
